0cca3661a1f83f18c1e244857fb95695c7be8f85
Sean Kramer <skramer@draper.com>
2019-12-04 12:02:17 -0500

    split nodes into two vectors based on parent or child status

--- src/d7/part1_2.cpp
+++ src/d7/part1_2.cpp
@@ -2,15 +2,16 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
-#include <boost/algorithm/string.hpp>
+#include <algorithm>
 
 #define ARROW_DELIM "->"
 
 struct Node
 {
 	int weight;
+    int length;
 	std::string name;
-	std::string node_str;
+    std::vector<std::string> vec_chld;
 	struct Node *nxt;
 };
 
@@ -31,60 +32,60 @@ void find_parent_node(Node *node, std::string pname, std::vector<Node> vec_node)
 
 }
 
-void get_node_attributes(std::string node_str, Node *node)
+void get_node_attributes(std::string node_str, Node& node)
 {
 	int st_paren, end_paren;
 	st_paren = node_str.find("(") + 1;
 	end_paren = node_str.find(")");
 	std::stringstream ss(node_str.substr(st_paren, end_paren - st_paren));
-	ss >> node->weight;
-	node->name = node_str.substr(0, node_str.find(' '));
-	node->nxt = NULL;
+	ss >> node.weight;
+    node.length = 1;
+	node.name = node_str.substr(0, node_str.find(' '));
+	node.nxt = NULL;
 }
 
-void create_node_ll(Node *llnode, std::vector<Node> vec_node)
+std::vector<std::string> get_children(std::string node_str)
 {
 	std::vector<std::string> nxt_name;
-	int arr_pos = llnode->node_str.find(ARROW_DELIM);
-	if (arr_pos <= 0) {
-		return;
-	} else {
-		std::stringstream ss(llnode->node_str.substr(arr_pos + sizeof(ARROW_DELIM)));
+    int arr_pos = node_str.find(ARROW_DELIM);
+    if (arr_pos > 0) {
+		std::stringstream ss(node_str.substr(arr_pos + sizeof(ARROW_DELIM)));
 		while(ss.good()) {
 			std::string sbstr;
 			getline(ss, sbstr, ',');
-			boost::trim_left(sbstr);
+            sbstr.erase(std::remove(sbstr.begin(), sbstr.end(), ' '), sbstr.end());
 			nxt_name.push_back(sbstr);
 		}
 	}
-}
-
-void printList(Node *node)  
-{  
-    while (node != NULL) {  
-        std::cout << node->name << " -> ";  
-        node = node->nxt;  
-    }
-    std::cout << std::endl;
+    return nxt_name;
 }
 
 int main(int argc, char **argv)
 {
 	std::string node_str;
 	std::ifstream input_file(argv[1]);
-	std::vector<Node> vec_node;
+    std::vector<Node> pnode, cnode;
+	Node node;
 
 	if (input_file.is_open()) {
 		while(getline(input_file, node_str)) {
-			Node *node = new Node();
-			node->node_str = node_str;
 			get_node_attributes(node_str, node);
-			vec_node.push_back(node);
+            node.vec_chld = get_children(node_str);
+            if (node.vec_chld.empty())
+                cnode.push_back(node);
+            else
+                pnode.push_back(node);
 		}
 	} else {
 		std::cout << "error: cannot open file" << std::endl;
 		exit(1);
 	}
+
+    while (!cnode.empty()) {
+        Node nit = cnode.front();
+        std::cout << nit.name << " (" << nit.weight  << ")" << std::endl;
+        cnode.erase(cnode.begin());
+    }
 
 	input_file.close();
 	return 0;
