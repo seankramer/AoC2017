290c477e51dd54adeeaabd1408912e0e27b05e16
Sean Kramer <sqk0624@draper.com>
2019-12-04 16:20:19 -0500

    completed part 1

--- src/d7/part1_2.cpp
+++ src/d7/part1_2.cpp
@@ -1,91 +1,145 @@
+#include <utility>
 #include <vector>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <algorithm>
 
+#define DEBUG
 #define ARROW_DELIM "->"
 
 struct Node
 {
 	int weight;
-    int length;
+	int lvl_weight;
 	std::string name;
-    std::vector<std::string> vec_chld;
-	struct Node *nxt;
+	std::vector<std::string> vec_chld;
 };
 
-// Node find_parent_node(std::string pname, std::vector<Node*> vec_node)
-// {
-// 	Node node;
-// 	for (auto it = vec_node.begin(); it != vec_node.end(); ++it) {
-// 		if (pname.compare((*it)->name) == 0) {
-// 			std::cout << (*it)->name << std::endl;
-// 			return *(*it);
-// 		}
-// 	}
-// 	return node;
-// }
-
-void find_parent_node(Node *node, std::string pname, std::vector<Node> vec_node)
+#ifdef DEBUG
+void print_node(Node *node)
 {
-
+	std::cout << node->name << " (" << node->weight << ")" << std::endl;
+	for (auto str : node->vec_chld)
+		std::cout << "    " << str << std::endl;
+	std::cout << std::endl << std::endl;
 }
 
-void get_node_attributes(std::string node_str, Node& node)
+// void print_vectors(std::vector<Node*> cnode, std::vector<Node*> pnode)
+void print_vectors(std::vector<Node*> pnode)
+{
+	// for (auto& c : cnode)
+	// 	std::cout << c->name << std::endl;
+	// std::cout << std::endl;
+
+	for (auto& p : pnode)
+		std::cout << p->name  << " " << p->weight << std::endl;
+	std::cout << std::endl;
+}
+
+void print_parent_vector(std::vector<Node*> pnode)
+{
+	for (auto& nd : pnode) {
+		std::cout << nd->name << " (" << nd->weight  << ") -> ";
+		for (auto chld : nd->vec_chld)
+			std::cout << chld << "; ";
+		std::cout << std::endl;
+	}
+}
+#endif
+
+std::pair<Node*, int> link_nodes(std::vector<Node*> vec_cnode, std::string cname)
+{
+	int ndx = 0;
+	for (auto& cnode : vec_cnode) {
+		if (cname.compare(cnode->name) == 0)
+			return std::make_pair(cnode, ndx);
+		ndx++;
+	}
+	return std::make_pair(nullptr, -1);
+}
+
+void get_attrib(std::string node_str, Node *node)
 {
 	int st_paren, end_paren;
 	st_paren = node_str.find("(") + 1;
 	end_paren = node_str.find(")");
 	std::stringstream ss(node_str.substr(st_paren, end_paren - st_paren));
-	ss >> node.weight;
-    node.length = 1;
-	node.name = node_str.substr(0, node_str.find(' '));
-	node.nxt = NULL;
+	ss >> node->weight;
+	node->lvl_weight = 0;
+	node->name = node_str.substr(0, node_str.find(' '));
 }
 
-std::vector<std::string> get_children(std::string node_str)
+std::vector<std::string> get_chld_names(std::string node_str)
 {
 	std::vector<std::string> nxt_name;
-    int arr_pos = node_str.find(ARROW_DELIM);
-    if (arr_pos > 0) {
+	int arr_pos = node_str.find(ARROW_DELIM);
+	if (arr_pos > 0) {
 		std::stringstream ss(node_str.substr(arr_pos + sizeof(ARROW_DELIM)));
 		while(ss.good()) {
 			std::string sbstr;
 			getline(ss, sbstr, ',');
-            sbstr.erase(std::remove(sbstr.begin(), sbstr.end(), ' '), sbstr.end());
+			sbstr.erase(std::remove(sbstr.begin(), sbstr.end(), ' '), sbstr.end());
 			nxt_name.push_back(sbstr);
 		}
 	}
-    return nxt_name;
+	return nxt_name;
 }
 
 int main(int argc, char **argv)
 {
 	std::string node_str;
 	std::ifstream input_file(argv[1]);
-    std::vector<Node> pnode, cnode;
-	Node node;
+	std::vector<Node*> pnode, cnode;
 
 	if (input_file.is_open()) {
 		while(getline(input_file, node_str)) {
-			get_node_attributes(node_str, node);
-            node.vec_chld = get_children(node_str);
-            if (node.vec_chld.empty())
-                cnode.push_back(node);
-            else
-                pnode.push_back(node);
+			Node *node = new Node();
+			get_attrib(node_str, node);
+			node->vec_chld = get_chld_names(node_str);
+			(node->vec_chld.empty()) ? cnode.push_back(node) : pnode.push_back(node);
+
 		}
 	} else {
 		std::cout << "error: cannot open file" << std::endl;
 		exit(1);
 	}
 
-    while (!cnode.empty()) {
-        Node nit = cnode.front();
-        std::cout << nit.name << " (" << nit.weight  << ")" << std::endl;
-        cnode.erase(cnode.begin());
-    }
+	while (!pnode.empty()) {
+		Node *node = pnode.front();
+		std::pair<Node*, int> cnode_pair;
+		std::cout << node->name << " (" << node->weight  << ")" << std::endl;
+
+		while (!node->vec_chld.empty()) {
+			std::string chld = node->vec_chld.front();
+			cnode_pair = link_nodes(cnode, chld);
+
+			if (cnode_pair.first != nullptr) {
+				// if (node->lvl_weight == 0) {
+				// 	node->lvl_weight = cnode_pair.first->weight;
+				// } else if (node->lvl_weight != cnode_pair.first->weight) {
+				// 	std::cout << node->name << " " << node->lvl_weight << " != ";
+				// 	std::cout << cnode_pair.first->name << " ";
+				// 	std::cout << cnode_pair.first->weight << std::endl;
+				// }
+				node->weight += cnode_pair.first->weight;
+				std::cout << "    " << cnode_pair.first->name << " (";
+				std::cout << cnode_pair.first->weight << ")" << std::endl;
+				cnode.erase(cnode.begin() + cnode_pair.second);
+				node->vec_chld.erase(node->vec_chld.begin());
+			} else {
+				pnode.push_back(node);
+				std::cout << "    child node not found" << std::endl;
+				break;
+			}
+		}
+		if (node->vec_chld.empty())
+			cnode.push_back(node);
+
+		if (pnode.size() == 1)
+			std::cout << "king node: " << node->name << std::endl;
+		pnode.erase(pnode.begin());
+	}
 
 	input_file.close();
 	return 0;
