2fdbb370b38e727fb4c62cc7095ef5be61fe80bb
Sean Kramer <sqk0624@draper.com>
2019-12-16 16:47:20 -0500

    parts 1 and 2 complete

--- src/d8/part1_2.cpp
+++ src/d8/part1_2.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 	}
 
 	std::string in_str;
-	int op_val, cmp_val;
+	int op_val, cmp_val, p1mx = 0, p2mx = 0;
 	std::string reg, op, cmp_reg, cmp;
 	std::unordered_map<std::string, int> reg_data;
 
@@ -31,19 +31,20 @@ int main(int argc, char **argv)
 		std::stringstream ss(in_str);
 		ss >> reg >> op >> op_val >> cmp_reg >> cmp_reg >> cmp >> cmp_val;
 		op_val = (op == "dec") ? op_val * -1 : op_val;
-		if (cmpr.at(cmp)(reg_data[cmp_reg], cmp_val))
+		if (cmpr.at(cmp)(reg_data[cmp_reg], cmp_val)) {
 			reg_data[reg] += op_val;
+			p2mx = (reg_data[reg] > p2mx) ? reg_data[reg] : p2mx;
+		}
 	}
 
-#ifdef PART1
-	int mx = 0;
 	std::unordered_map<std::string, int>::iterator it = reg_data.begin();
 	while (it != reg_data.end()) {
-		mx = (it->second > mx) ? it->second : mx;
+		p1mx = (it->second > p1mx) ? it->second : p1mx;
 		it++;
 	}
-	std::cout << mx << std::endl;
-#endif
+
+	std::cout << "part 1: " << p1mx << std::endl;
+	std::cout << "part 2: " << p2mx << std::endl;
 
 	return 0;
 }
