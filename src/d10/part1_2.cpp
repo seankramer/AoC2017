f126bfd8482d8166671cd36adf8375b123758425
Sean Kramer <sqk0624@draper.com>
2019-12-18 10:03:20 -0500

    fixed errs in part 1 -- complete

--- src/d10/part1_2.cpp
+++ src/d10/part1_2.cpp
@@ -4,7 +4,8 @@
 #include <sstream>
 #include <iostream>
 
-#define TEST_LEN 10
+// #define EXAMPLE
+#define TEST_LEN 5
 #define MAX_LEN 256
 
 void populate_vec_rev(std::ifstream& infile, std::vector<int>& vec)
@@ -20,22 +21,6 @@ void populate_vec_rev(std::ifstream& infile, std::vector<int>& vec)
 	}
 }
 
-int rev_sublist(std::vector<int>& vec, int rev_len, int& ndx, int skip)
-{
-	std::cout << ndx << " & " << rev_len << std::endl;
-	for (int i = 0; i <= rev_len / 2; i++) {
-		if (ndx + i <= vec.size())
-			std::swap(vec[(rev_len / 2) - i], vec[(rev_len / 2) + i]);
-		else
-			exit(1);
-	}
-
-	for (int i = 0; i < vec.size(); i++)
-		std::cout << vec[i] << "; ";
-	std::cout << std::endl << std::endl;
-	return rev_len + skip;
-}
-
 int main(int argc, char **argv)
 {
 	std::ifstream input_file(argv[1]);
@@ -44,20 +29,29 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	int skip = 0, ndx = 0;
+	int ndx = 0, ldex, hdex;;
 	std::vector<int> vec_rev;
+
+#ifdef EXAMPLE
+	std::vector<int> n_seq(TEST_LEN);
+#else
 	std::vector<int> n_seq(MAX_LEN);
+#endif
+
 	populate_vec_rev(input_file, vec_rev);
 	std::iota(n_seq.begin(), n_seq.end(), 0);
 
-	// for (int i = 0; i < n_seq.size(); i++)
-	// 	std::cout << n_seq[i] << "; ";
-	// std::cout << std::endl << std::endl;
-
 	for (int i = 0; i < vec_rev.size(); i++) {
-		ndx += rev_sublist(n_seq, vec_rev[i], ndx, skip);
-		skip++;
+		for (int j = 0; j < vec_rev[i] / 2; j++) {
+			ldex = (ndx + j) % n_seq.size();
+			hdex = (ndx + vec_rev[i] - 1 - j) % n_seq.size();
+			std::swap(n_seq[ldex], n_seq[hdex]);
+		}
+		ndx += vec_rev[i] + i;
 	}
+	std::cout << "part 1 -> " << n_seq[0] * n_seq[1] << std::endl;
+
+
 
 	input_file.close();
 	return 0;
